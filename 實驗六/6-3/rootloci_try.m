036a11cd9a48e67f56a2cd2b8fd1f6ec45bf062d
yeyi58095 <yeyi58095@gmail.com>
2025-04-03 17:08:46 +0800

    try 6-3 and making ppt

--- 實驗六/6-3/rootloci_try.m
+++ 實驗六/6-3/rootloci_try.m
@@ -1,14 +1,14 @@
 s = tf('s');
-a = 1;
-b = 2;
+tau = 0.51;
+K_m  = 307;
 D = 3;
 
-sys = (s + D) / (s * (s + a));
-
+sys =  (s + D) / (s * (tau *s + 1));
+%{
 figure(1)
 rlocus(sys)
-
+%}
 figure(2)
-K = 9.9;
+K = 3.86;
 sys_cl = feedback(K * sys, 1);
 step(sys_cl)
