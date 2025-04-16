a02de6adf6cd0ce30be9084b9cf44e3025f8d0f5
yeyi58095 <yeyi58095@gmail.com>
2025-04-16 14:59:45 +0800

    lab 8 sim finish

---
+++ 實驗八/8-1/0.6/simulink/PointSix.m
@@ -0,0 +1,9 @@
+clear
+clc
+model = 'pointSix_Slx';  % 你的模型名稱
+open_system(model);
+io = [linio([model '/Gain3'],1,'input');      % 你可以根據 block name 修改
+      linio([model '/Transfer Fcn1'],1,'output')];
+linsys = linearize(model, io);
+bode(linsys), grid on
+title('Bode Plot of Transfer Function')
